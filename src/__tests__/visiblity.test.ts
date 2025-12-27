be4a17e64ec880445ebd68feec2f50417e95b8fa
IceFreez3r <39794212+IceFreez3r@users.noreply.github.com>
2025-12-27 13:25:22 +0100

    Atan2 cache with unit tests

--- src/__tests__/visiblity.test.ts
+++ src/__tests__/visiblity.test.ts
@@ -1,53 +1,85 @@
-import { describe, expect, it } from "vitest";
-import { getInterval } from "../visibility";
+import { describe, expect, it, vi } from "vitest";
+import { Atan2 } from "../visibility";
 
-describe("getInterval", () => {
-    // ! REMINDER: Argument order of Math.atan2(y, x)
-    it("should return the correct angle interval for [1, 0]", () => {
-        const interval = getInterval(1, 0);
-        expect(interval[0]).toBeCloseTo(Math.atan2(-0.5, 0.5)); // bottom-left
-        expect(interval[1]).toBeCloseTo(Math.atan2(0.5, 0.5)); // top-left
+describe("Atan2 cache", () => {
+    describe("getInterval", () => {
+        const atan2 = new Atan2(2, 2);
+
+        // ! REMINDER: Argument order of Math.atan2(y, x)
+        it("should return the correct angle interval for [1, 0]", () => {
+            const interval = atan2.getInterval(1, 0);
+            expect(interval[0]).toBeCloseTo(Math.atan2(-0.5, 0.5)); // bottom-left
+            expect(interval[1]).toBeCloseTo(Math.atan2(0.5, 0.5)); // top-left
+        });
+
+        it("should return the correct angle interval for [0, 1]", () => {
+            const interval = atan2.getInterval(0, 1);
+            expect(interval[0]).toBeCloseTo(Math.atan2(0.5, 0.5)); // bottom-right
+            expect(interval[1]).toBeCloseTo(Math.atan2(0.5, -0.5)); // bottom-left
+        });
+
+        it("should return the correct angle interval for [-1, 0] (wrap-around)", () => {
+            const interval = atan2.getInterval(-1, 0);
+            expect(interval[0]).toBeCloseTo(Math.atan2(0.5, -0.5)); // top-right
+            expect(interval[1]).toBeCloseTo(Math.atan2(-0.5, -0.5)); // bottom-right
+        });
+
+        it("should return the correct angle interval for [0, -1]", () => {
+            const interval = atan2.getInterval(0, -1);
+            expect(interval[0]).toBeCloseTo(Math.atan2(-0.5, -0.5)); // top-left
+            expect(interval[1]).toBeCloseTo(Math.atan2(-0.5, 0.5)); // top-right
+        });
+
+        it("should return the correct angle interval for [1, 1]", () => {
+            const interval = atan2.getInterval(1, 1);
+            expect(interval[0]).toBeCloseTo(Math.atan2(0.5, 1.5)); // bottom-right
+            expect(interval[1]).toBeCloseTo(Math.atan2(1.5, 0.5)); // top-left
+        });
+
+        it("should return the correct angle interval for [-1, 1]", () => {
+            const interval = atan2.getInterval(-1, 1);
+            expect(interval[0]).toBeCloseTo(Math.atan2(1.5, -0.5)); // top-right
+            expect(interval[1]).toBeCloseTo(Math.atan2(0.5, -1.5)); // bottom-left
+        });
+
+        it("should return the correct angle interval for [-1, -1]", () => {
+            const interval = atan2.getInterval(-1, -1);
+            expect(interval[0]).toBeCloseTo(Math.atan2(-0.5, -1.5)); // top-left
+            expect(interval[1]).toBeCloseTo(Math.atan2(-1.5, -0.5)); // bottom-right
+        });
+
+        it("should return the correct angle interval for [1, -1]", () => {
+            const interval = atan2.getInterval(1, -1);
+            expect(interval[0]).toBeCloseTo(Math.atan2(-1.5, 0.5)); // bottom-left
+            expect(interval[1]).toBeCloseTo(Math.atan2(-0.5, 1.5)); // top-right
+        });
     });
 
-    it("should return the correct angle interval for [0, 1]", () => {
-        const interval = getInterval(0, 1);
-        expect(interval[0]).toBeCloseTo(Math.atan2(0.5, 0.5)); // bottom-right
-        expect(interval[1]).toBeCloseTo(Math.atan2(0.5, -0.5)); // bottom-left
-    });
-
-    it("should return the correct angle interval for [-1, 0] (wrap-around)", () => {
-        const interval = getInterval(-1, 0);
-        expect(interval[0]).toBeCloseTo(Math.atan2(0.5, -0.5)); // top-right
-        expect(interval[1]).toBeCloseTo(Math.atan2(-0.5, -0.5)); // bottom-right
-    });
-
-    it("should return the correct angle interval for [0, -1]", () => {
-        const interval = getInterval(0, -1);
-        expect(interval[0]).toBeCloseTo(Math.atan2(-0.5, -0.5)); // top-left
-        expect(interval[1]).toBeCloseTo(Math.atan2(-0.5, 0.5)); // top-right
-    });
-
-    it("should return the correct angle interval for [1, 1]", () => {
-        const interval = getInterval(1, 1);
-        expect(interval[0]).toBeCloseTo(Math.atan2(0.5, 1.5)); // bottom-right
-        expect(interval[1]).toBeCloseTo(Math.atan2(1.5, 0.5)); // top-left
-    });
-
-    it("should return the correct angle interval for [-1, 1]", () => {
-        const interval = getInterval(-1, 1);
-        expect(interval[0]).toBeCloseTo(Math.atan2(1.5, -0.5)); // top-right
-        expect(interval[1]).toBeCloseTo(Math.atan2(0.5, -1.5)); // bottom-left
-    });
-
-    it("should return the correct angle interval for [-1, -1]", () => {
-        const interval = getInterval(-1, -1);
-        expect(interval[0]).toBeCloseTo(Math.atan2(-0.5, -1.5)); // top-left
-        expect(interval[1]).toBeCloseTo(Math.atan2(-1.5, -0.5)); // bottom-right
-    });
-
-    it("should return the correct angle interval for [1, -1]", () => {
-        const interval = getInterval(1, -1);
-        expect(interval[0]).toBeCloseTo(Math.atan2(-1.5, 0.5)); // bottom-left
-        expect(interval[1]).toBeCloseTo(Math.atan2(-0.5, 1.5)); // top-right
-    });
+    [2, 3, 4].map((size) =>
+        it(`should return consistent results for a ${size}x${size} grid`, () => {
+            const atan2Spy = vi.spyOn(Math, "atan2");
+            const atan2 = new Atan2(size, size);
+            expect(atan2Spy).toHaveBeenCalledTimes(2 * size * (2 * size));
+            atan2Spy.mockClear();
+            for (let dy = -1; dy <= 1; dy++) {
+                for (let dx = -1; dx <= 1; dx++) {
+                    const corners = [
+                        [dx - 0.5, dy - 0.5], // top-left
+                        [dx + 0.5, dy - 0.5], // top-right
+                        [dx + 0.5, dy + 0.5], // bottom-right
+                        [dx - 0.5, dy + 0.5], // bottom-left
+                    ];
+                    const cached = atan2.getXY(dx, dy);
+                    corners.forEach(([x, y], index) => {
+                        const expected = Math.atan2(y, x);
+                        const actual = cached[index];
+                        expect(actual).toBeCloseTo(expected);
+                    });
+                }
+            }
+            // only the test calls, others should be cached
+            expect(atan2Spy).toHaveBeenCalledTimes(4 * 3 * 3);
+            atan2Spy.mockReset();
+        })
+    );
 });
--- src/visibility.ts
+++ src/visibility.ts
@@ -1,17 +1,63 @@
-/**
- * @returns the angle intervals of the pair of corners that block the most vision
- */
-export function getInterval(dx: number, dy: number): [number, number] {
-    const corners = [
-        [dx - 0.5, dy - 0.5], // top-left
-        [dx + 0.5, dy - 0.5], // top-right
-        [dx + 0.5, dy + 0.5], // bottom-right
-        [dx - 0.5, dy + 0.5], // bottom-left
-    ];
+export class Atan2 {
+    cache: number[];
+    width: number;
+    effectiveWidth: number;
+    height: number;
+    effectiveHeight: number;
 
-    const angles = corners.map(([cx, cy]) => Math.atan2(cy, cx)); // TODO: Cache atan2
-    angles.sort((a, b) => a - b); // Sort ascending
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        this.effectiveWidth = 2 * width;
+        this.effectiveHeight = 2 * height;
+        this.cache = new Array(this.effectiveWidth * this.effectiveHeight);
 
-    const wrapAround = angles[3] - angles[0] > Math.PI;
-    return wrapAround ? [angles[2], angles[1]] : [angles[0], angles[3]];
+        // Precompute all possible corners
+        let index = 0;
+        for (let cy = -height + 0.5; cy <= height - 0.5; cy++) {
+            for (let cx = -width + 0.5; cx <= width - 0.5; cx++, index++) {
+                this.cache[index] = Math.atan2(cy, cx);
+            }
+        }
+    }
+
+    getYX(dy: number, dx: number): [number, number, number, number] {
+        const topLeftIndex = (dy + this.height - 1) * this.effectiveWidth + (dx + this.width - 1);
+        const topRightIndex = topLeftIndex + 1;
+        const bottomLeftIndex = topLeftIndex + this.effectiveWidth;
+        const bottomRightIndex = bottomLeftIndex + 1;
+        if (this.cache[topLeftIndex] === undefined) {
+            this.cache[topLeftIndex] = Math.atan2(dy - 0.5, dx - 0.5);
+        }
+        if (this.cache[topRightIndex] === undefined) {
+            this.cache[topRightIndex] = Math.atan2(dy - 0.5, dx + 0.5);
+        }
+        if (this.cache[bottomLeftIndex] === undefined) {
+            this.cache[bottomLeftIndex] = Math.atan2(dy + 0.5, dx - 0.5);
+        }
+        if (this.cache[bottomRightIndex] === undefined) {
+            this.cache[bottomRightIndex] = Math.atan2(dy + 0.5, dx + 0.5);
+        }
+        return [
+            this.cache[topLeftIndex],
+            this.cache[topRightIndex],
+            this.cache[bottomRightIndex],
+            this.cache[bottomLeftIndex],
+        ];
+    }
+
+    getXY(dx: number, dy: number): [number, number, number, number] {
+        return this.getYX(dy, dx);
+    }
+
+    /**
+     * @returns the angle intervals of the pair of corners that block the most vision
+     */
+    getInterval(dx: number, dy: number): [number, number] {
+        const angles = this.getYX(dy, dx);
+        angles.sort((a, b) => a - b); // Sort ascending
+
+        const wrapAround = angles[3] - angles[0] > Math.PI;
+        return wrapAround ? [angles[2], angles[1]] : [angles[0], angles[3]];
+    }
 }
